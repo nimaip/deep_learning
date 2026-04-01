f9475dcf1e19d6b0ce28707f2e662cce8ed1b867
Nimai Ponna <nimaipponna@gmail.com>
2026-03-31 23:30:39 -0400

    Created StockRNN class

--- RNNs/rnn_stock_prediction.py
+++ RNNs/rnn_stock_prediction.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 from torch.utils.data import TensorDataset, DataLoader
 
@@ -40,3 +42,23 @@ test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
 # print(f"X_train shape: {X_train.shape}")
 # print(f"y_train shape: {y_train.shape}") 
+
+
+class StockRNN(nn.Module):
+    def __init__(self, input_size=1, hidden_size=32, num_layers=1, output_size=1):
+        super(StockRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, hn = self.rnn(x, h0)
+        final_time_step_out = out[:, -1, :]
+        prediction = self.fc(final_time_step_out)
+        return prediction
+
+model = StockRNN(input_size=1, hidden_size=32, num_layers=1, output_size=1)
+
+
