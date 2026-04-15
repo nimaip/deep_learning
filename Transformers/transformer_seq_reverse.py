8a96b3748511c5878329199bd44cfaf2bdd4ae52
Nimai Ponna <nimaipponna@gmail.com>
2026-04-15 00:13:20 -0400

    Created trasformer class and forward function

--- Transformers/transformer_seq_reverse.py
+++ Transformers/transformer_seq_reverse.py
@@ -28,3 +28,27 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:x.size(1), :]
         return x
+
+class TransformerModel(nn.Module):
+    def __init__(self, vocab_size, d_model, nhead, num_layers, dim_feedforward):
+        super().__init__()
+        self.d_model = d_model
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.pos_encoder = PositionalEncoding(d_model)
+        
+        self.transformer = nn.Transformer(
+            d_model=d_model, 
+            nhead=nhead, 
+            num_encoder_layers=num_layers, 
+            num_decoder_layers=num_layers, 
+            dim_feedforward=dim_feedforward,
+            batch_first=True
+        )
+        
+        self.out = nn.Linear(d_model, vocab_size)
+
+    def forward(self, src, tgt, tgt_mask=None):
+        src = self.pos_encoder(self.embedding(src) * math.sqrt(self.d_model))
+        tgt = self.pos_encoder(self.embedding(tgt) * math.sqrt(self.d_model))
+        output = self.transformer(src, tgt, tgt_mask=tgt_mask)
+        return self.out(output)
